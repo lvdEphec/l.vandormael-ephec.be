2a62ecfb704470b6aa14cd3258ae0dcc00dac240
lvdEphec <33726310+lvdEphec@users.noreply.github.com>
2020-04-11 12:25:37 +0200

    Retirer tabulations

--- js/ajax-pokeapi-fr.js
+++ js/ajax-pokeapi-fr.js
@@ -3,19 +3,22 @@
 /* lvd Pâques 2020 */
 /* Script pour tester des requêtes en utilisant ajax et pour construire dynmiquement du code HTML */
 
-/* Ce code va récupérer les noms en français,
+/* 
+Ce code va récupérer les noms en français,
 trie sur base des noms 
 et affiche toute la liste des espèces en une fois.
-Il utilise fetch et Promise.all pour cela. Il est possible de s'en passer et d'utiliser xhr. */
+Il utilise fetch et Promise.all pour cela. Il est possible de s'en passer et d'utiliser xhr. 
+TODO: JSDOC 
+*/
 
 
-/************ CONSTANTES et variables globales **************/
+/* *********** CONSTANTES et variables globales ************* */
 const POKE_API_URL_HABITAT = "https://pokeapi.co/api/v2/pokemon-habitat";
 
 let liensEspecesHabitats = []; // [ {id: , nom: , urlsEspecesPokemons: }, ]
 
 
-/************ Gestions du chargement initial de la page **************/
+/* *********** Gestions du chargement initial de la page ************* */
 
 //window.addEventListener('load', initialiser);
 document.addEventListener('DOMContentLoaded', initialiser);
@@ -27,72 +30,81 @@ function initialiser() {
     chargerHabitats();
 }
 
-/************ Chargement de la liste des choix s'habitats **************/
+
+/* *********** Chargement de la liste des choix d'habitats ************* */
 
 function chargerHabitats() {
 	
-	fetch(POKE_API_URL_HABITAT)
-	.then(reponseJson => reponseJson.json())
-	.then(reponse => recupererHabitatsfr(reponse.results.map(x => x.url)))
-	.then(habitats => {
-		remplirListeHabitats(habitats); 
-		// après chargement, enclenche un "change" pour afficher directement les espèces de l'option sélectionnée par défaut en premier
-		document.getElementById("choixHabitat").dispatchEvent(new Event('change')); 
-		// chargerEspeces.call(document.getElementById("choixHabitat")); // même chose
-		})
-	.catch(error => {console.log("Y a un problème ! : " + error.message);});
+    fetch(POKE_API_URL_HABITAT)
+    .then(reponseJson => reponseJson.json())
+    .then(reponse => recupererHabitatsfr(reponse.results.map(x => x.url)))
+    .then(habitats => {
+        remplirListeHabitats(habitats); 
+        // enclenche un "change" pour afficher directement les espèces de l'option en premier
+        document.getElementById("choixHabitat").dispatchEvent(new Event('change')); 
+        // chargerEspeces.call(document.getElementById("choixHabitat")); // même chose
+        }
+    )
+    .catch(error => {console.log("Y a un problème ! : " + error.message);});
 };
 	
 
 function recupererHabitatsfr(habitatsUrls) {
     try {		
-		return Promise.all(
-            habitatsUrls.map( url => fetch(url)
-									.then(reponse => reponse.json())
-									.then(habitats => { 
-										let habitat = {};
-										habitat.id = habitats.id;
-					                    habitat.nom = habitats.names.filter(x => x.language.name === "fr")[0].name;
-										habitat.urlsEspecesPokemons = habitats.pokemon_species.map(x => x.url);
-										return habitat;
-					                })
-							)
-		)						
-	}
+        return Promise.all(
+            habitatsUrls.map( url => 
+                fetch(url)
+                .then(reponse => reponse.json())
+                .then(habitats => { 
+                    let habitat = {};
+                    habitat.id = habitats.id;
+                    habitat.nom = habitats.names.filter(x => x.language.name === "fr")[0].name;
+                    habitat.urlsEspecesPokemons = habitats.pokemon_species.map(x => x.url);
+                    return habitat;
+                    }
+                )
+            )
+        )						
+    }
     catch(err) {
         console.warn(err);
     };
 }
 
 function remplirListeHabitats(habitats) {
-    liensEspecesHabitats = [...habitats].sort( (x, y) => x.id - y.id ); // enregistre les données, évite de devoir rappeler 
+    // enregistre les données, notamment url du detail des especes
+    // pour pouvoir appeler dès que nécessaire sans de devoir rappeler
+    liensEspecesHabitats = [...habitats].sort( (x, y) => x.id - y.id );  
+	
     habitats.sort( (x, y) => x.nom.localeCompare(y.nom) ); 
-	let options = "";
-	for (let habitat of habitats) {
-		options += `<option value="${habitat.id}">${habitat.nom}</option>`;
-	}
-	document.getElementById("choixHabitat").innerHTML = options;
+    let options = "";
+    for (let habitat of habitats) {
+        options += `<option value="${habitat.id}">${habitat.nom}</option>`;
+    }
+    document.getElementById("choixHabitat").innerHTML = options;
 }
 
 
-/************ Affichage des especes de l'habitat sélectionné **************/
+/* *********** Affichage des especes de l'habitat sélectionné ************* */
 
 function chargerEspeces() {
     document.getElementById("listeEspeces").innerHTML = "";
-	// urls = liensEspecesHabitats[this.value]; normalement ok, mais on fait safe
-	const urls = liensEspecesHabitats.filter(x => (x.id == this.value))[0].urlsEspecesPokemons; // là confiance, ce ne sera pas vide :)
+    // urls = liensEspecesHabitats[this.value]; normalement ok, mais on fait safe on va aller le rechercher
+	// ci-dessous on fait confiance, ce ne sera pas vide :)
+    const urls = liensEspecesHabitats.filter(x => (x.id == this.value))[0].urlsEspecesPokemons; 
 	
-	try {		
-		Promise.all(
-            urls.map( url => fetch(url)
-							.then(reponseJson => reponseJson.json())
-							.then(espece => {										
-								return espece.names.filter(x => x.language.name === "fr")[0].name;
-							})
-					)
-		)										
-		.then (nomsEspeces => {ajouterEspeces(nomsEspeces) } );
-	}
+    try {		
+        Promise.all(
+            urls.map( url => 
+                fetch(url)
+                .then(reponseJson => reponseJson.json())
+                .then(espece => {										
+                    return espece.names.filter(x => x.language.name === "fr")[0].name;
+                })
+            )
+        )										
+        .then (nomsEspeces => {ajouterEspeces(nomsEspeces) } );
+    }
     catch(err) {
         console.warn(err);
     };
@@ -100,9 +112,9 @@ function chargerEspeces() {
 }
 
 function ajouterEspeces(nomsEspeces) {
-	let liElements = "";
-	for (let nomEspece of nomsEspeces.sort((x, y) => x.localeCompare(y))) {
-		liElements += "<li>" + nomEspece + "</li>";
-	}
+    let liElements = "";
+    for (let nomEspece of nomsEspeces.sort((x, y) => x.localeCompare(y))) {
+        liElements += "<li>" + nomEspece + "</li>";
+    }
     document.getElementById("listeEspeces").innerHTML = liElements;
 }
